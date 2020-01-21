806262cc8bcdaa4ea41c7477e2cf2dbe54759e51
Bhagyashri Padmawar <bhagyashri.padmawar@gmail.com>
2020-01-21 09:56:24 -0500

    add prod.js

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClient: process.env.GOOGLE_CLIENT_ID,
+  googleSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+};
