ea4d397d3ed3c3126fb4cbf1bcffcf755c696cec
Bhagyashri Padmawar <bhagyashri.padmawar@gmail.com>
2020-01-21 10:03:31 -0500

    solving deploy issues

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-  googleClient: process.env.GOOGLE_CLIENT_ID,
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY
