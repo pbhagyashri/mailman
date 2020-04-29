7f99b35fd2c90fb7a703dbd37add12e3cd66f98a
Bhagyashri Padmawar <bhagyashri.padmawar@gmail.com>
2020-04-28 22:02:32 -0400

    Add react client

---
+++ client/src/components/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Landing = () => {
+	return (
+		<div style={{ textAlign: 'center' }}>
+			<h1>Mailbox!</h1>
+		</div>
+	)
+}
+
+export default Landing
--- services/passport.js
+++ services/passport.js
@@ -1,16 +1,16 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const mongoose = require('mongoose');
-const keys = require('../config/keys');
-const User = mongoose.model('users');
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const mongoose = require('mongoose')
+const keys = require('../config/keys')
+const User = mongoose.model('users')
 
 passport.serializeUser((user, done) => {
-	done(null, user.id);
-});
+	done(null, user.id)
+})
 
 passport.deserializeUser((id, done) => {
-	User.findById(id).then((user) => done(null, user));
-});
+	User.findById(id).then((user) => done(null, user))
+})
 
 passport.use(
 	new GoogleStrategy(
@@ -20,14 +20,12 @@ passport.use(
 			callbackURL: '/auth/google/callback',
 			proxy: true
 		},
-		(accessToken, refreshToken, profile, done) => {
-			User.findOne({ googleId: profile.id }).then((existingUser) => {
-				if (existingUser) {
-					done(null, existingUser);
-				} else {
-					new User({ googleId: profile.id }).save().then((user) => done(null, user));
-				}
-			});
+		async (accessToken, refreshToken, profile, done) => {
+			const existingUser = await User.findOne({ googleId: profile.id })
+			if (existingUser) done(null, existingUser)
+
+			const user = await new User({ googleId: profile.id }).save()
+			done(null, user)
 		}
 	)
-);
+)
